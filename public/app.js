32e1826e6577eba87d39d22002e9b138a1c0f449
amandam2017 <amandamaarman19911@gmail.com>
2021-11-26 16:34:46 +0200

    cars displays on cars.html

--- public/app.js
+++ public/app.js
@@ -7,30 +7,21 @@ const colorTemplateText = document.querySelector('.colorTemplate');
 // compile handlebars template
 const colorTemplete = Handlebars.compile(colorTemplateText.innerHTML);
 
-axios.get('http://api-tutor.herokuapp.com/v1/colors/')
+axios.get('https://api-tutor.herokuapp.com/v1/colors/')
 .then(function(result){
     console.log(result.data)
     color.innerHTML = colorTemplete({templateData:result.data})
 })
 
-axios.get('http://api-tutor.herokuapp.com/v1/makes')
+axios.get('https://api-tutor.herokuapp.com/v1/makes')
 .then(function(result){
     console.log(result.data)
     carMakes.innerHTML = colorTemplete({templateData:result.data})
 })
 
-axios.get('http://api-tutor.herokuapp.com/v1/cars')
+axios.get('https://api-tutor.herokuapp.com/v1/cars')
 .then(function(result){
     console.log(result.data)
     carsObj.innerHTML = colorTemplete({template:result.data})
 })
 
-
-
-
-
-
-
-
-
-// const color = document.createElement('li');
